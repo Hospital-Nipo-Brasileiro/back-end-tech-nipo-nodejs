cbdeaa548b48f6f29059b302d863ccc1e33d3a84
Sup Gustavo de Souza Fonseca <sup.gustavo@nipo.local>
2023-07-17 09:24:12 -0300

    Adicionando pasta de revisão e modificando modelo de estoque e migration e passando o id de estoque para zona

--- src/migrations/20230714133316-create-tn-t-estoque.js
+++ src/migrations/20230714133316-create-tn-t-estoque.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("TN_T_ESTOQUEs", {
+    await queryInterface.createTable("TN_T_ESTOQUE", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -31,6 +31,6 @@ module.exports = {
   },
   // eslint-disable-next-line no-unused-vars
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("TN_T_ESTOQUEs");
+    await queryInterface.dropTable("TN_T_ESTOQUE");
   }
 };
--- src/models/tn_t_zona.js
+++ src/models/tn_t_zona.js
@@ -1,23 +1,18 @@
-'use strict';
+"use strict";
 const {
   Model
-} = require('sequelize');
+} = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class TN_T_ZONA extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      TN_T_ZONA.belongsTo(models.TN_T_ESTOQUE, {foreignKey: "id_estoque"});
     }
   }
   TN_T_ZONA.init({
     id_estoque: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'TN_T_ZONA',
+    modelName: "TN_T_ZONA",
   });
   return TN_T_ZONA;
 };
