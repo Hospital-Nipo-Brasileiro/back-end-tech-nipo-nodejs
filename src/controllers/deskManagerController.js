cd9bd3aa02f1091212f850d235168c472a31a50e
Sup Gustavo de Souza Fonseca <sup.gustavo@nipo.local>
2023-08-11 16:23:13 -0300

    mudando looping de criacao de users

--- src/controllers/deskManagerController.js
+++ src/controllers/deskManagerController.js
@@ -1,17 +1,19 @@
 const axios = require("axios");
 const DeskManagerService = require("../services/DeskManagerService.js");
+require("dotenv").config(); // Carrega as variáveis de ambiente do arquivo
 
 class DeskManagerController {
        
   static async autenticar(req, res, next){
+    const OPERATOR_KEY = "1979133fd997e784df00f8d3a4855b9491baaac5";
+    const ENVIROMENT_KEY = "2f9e6f32abaaf94b6e161a556343e947020908f5";
 
-    const operatorKey = "1979133fd997e784df00f8d3a4855b9491baaac5"; //CHAVE DE OPERADOR MAURICIO
-    const enviromentKey = "2f9e6f32abaaf94b6e161a556343e947020908f5"; //CHAVE AMBIENTE
+    
 
     try{
       const response = await axios.post("https://api.desk.ms/Login/autenticar",
-        {PublicKey : enviromentKey},
-        {headers : {Authorization : operatorKey, JsonPath: true}}
+        {PublicKey : ENVIROMENT_KEY},
+        {headers : {Authorization : OPERATOR_KEY, JsonPath: true}}
       );
     
       if(response.status === 200 && response.data && response.data.access_token) { 
@@ -58,10 +60,8 @@ class DeskManagerController {
     try {
       const usuarios = await DeskManagerService.readCSV(file, res);
 
-      res.send(usuarios);
-
       // Armazena os usuários na requisição para uso posterior
-      req.fileData = usuarios; 
+      req.fileData = usuarios;     
       next();
 
     } catch (error) {
@@ -86,12 +86,19 @@ class DeskManagerController {
       // Prepare the headers with the authorization token
       const headers = { headers: { Authorization: token } };
 
+      const usuariosCriados = [];
       
-      // Fazer o PUT request para cada usuário individualmente
       for (const usuario of usuarios) {
-        const usuarioCriado = await axios.put(url, usuario, headers);
-        res.send(usuarioCriado);
+        try {
+          const usuarioCriado = await axios.put(url, usuario.TUsuario, headers);
+          usuariosCriados.push(usuarioCriado.data);
+        } catch (error) {
+          console.error(`Erro ao salvar usuário: ${error.message}`);
+          usuariosCriados.push({ error: `Erro ao salvar usuário: ${error.message}` });
+        }
       }
+  
+      res.json({ usuariosCriados });
 
     } catch (error) {
       next(`Erro ao salvar solicitante: ${error.message}`, 500);
