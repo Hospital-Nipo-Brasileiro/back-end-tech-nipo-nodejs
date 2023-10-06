fed823c45f51cda98553bc94abdce7e55618d596
GustavoDeSouzaFonseca <rm94067@fiap.com.br>
2023-10-06 20:31:05 -0300

    [:construction:] Adicionando vercel para o deploy

--- tests/login.test.js
+++ tests/login.test.js
@@ -7,15 +7,15 @@ describe("Deve ser passado todas as validações de login", () => {
 
   let _server = {};
   before(async () => {
-    _server = (await require("../../server.js")).app;
+    _server = (await require("../server.js")).app;
     await new Promise(resolve => _server.once("listening", resolve));
   });
-  
+
   after(done => _server.close(done));
 
   it("Deve barrar a passagem de uma criação de usuário", async () => {
     const data = {
-      ds_username:"Gustavofonseca",
+      ds_username: "Gustavofonseca",
       ds_password: "123"
     };
 
