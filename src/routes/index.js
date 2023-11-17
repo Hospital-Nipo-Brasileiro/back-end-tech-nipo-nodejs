55960dd598d366a94b2af67fa9721a7b0ecb912d
Sup Gustavo de Souza Fonseca <sup.gustavo@nipo.local>
2023-11-17 11:30:06 -0300

    [:fire:] removendo arquivos de develop

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,14 +4,6 @@ const cargos = require("./CargoRoute.js");
 const setores = require("./SetorRoute.js");
 const sistemas = require("./SistemasRoute.js");
 const cargoSetor = require("./CargoSetorRoute.js");
-const estoques = require("./EstoqueRoute.js");
-const zonas = require("./ZonaRoute.js");
-const armarios = require("./ArmarioRoute.js");
-const prateleiras = require("./PrateleirasRoute.js");
-const baus = require("./BauRoute.js");
-const itens = require("./ItemRoute.js");
-const itemGuardado = require("./ItemGuardadoRoute.js");
-const deskManager = require("./DeskManagerRoute.js");
 const admissaoCSV = require("./AdmissaoRoute.js");
 const login = require("./LoginRoute.js");
 const papeis = require("./papelRoute.js");
@@ -28,14 +20,6 @@ const routes = (app) => {
     setores,
     sistemas,
     cargoSetor,
-    estoques,
-    zonas,
-    armarios,
-    prateleiras,
-    baus,
-    itens,
-    itemGuardado,
-    deskManager,
     admissaoCSV,
     login,
     papeis,
