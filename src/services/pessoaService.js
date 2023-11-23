f235e6ebedf569bcbf33640cbfe05a4e1af7318e
Sup Gustavo de Souza Fonseca <sup.gustavo@nipo.local>
2023-11-23 16:20:51 -0300

    [:construction:] alterado query para buscar todos usuarios com ou sem sistemas

--- src/services/pessoaService.js
+++ src/services/pessoaService.js
@@ -4,19 +4,17 @@ class PessoaService {
   static async buscaSistemaPorPessoa() {
     try {
       const resultado = await db.sequelize.query(`
-        SELECT 
-          TN_T_PESSOA.id as ID,
-          TN_T_PESSOA.ds_nome AS NOME,
-          TN_T_SISTEMA.ds_nome as SISTEMA
-        FROM TN_T_PESSOA
-          LEFT JOIN TN_T_SISTEMA_PESSOA ON TN_T_SISTEMA_PESSOA.id_pessoa = TN_T_PESSOA.id
-          LEFT JOIN TN_T_SISTEMA ON TN_T_SISTEMA.id = TN_T_SISTEMA_PESSOA.id_sistema
-          AND TN_T_SISTEMA_PESSOA.dt_deleted IS NULL
-          AND TN_T_SISTEMA.dt_deleted IS NULL
-        GROUP BY
-          TN_T_PESSOA.id,
-          TN_T_PESSOA.ds_nome,
-          TN_T_SISTEMA.ds_nome
+      SELECT 
+        TN_T_PESSOA.id AS ID,
+        TN_T_PESSOA.ds_nome AS NOME,
+        COALESCE(STRING_AGG(TN_T_SISTEMA.ds_nome, ', '), '') AS SISTEMAS
+      FROM TN_T_PESSOA
+        LEFT JOIN TN_T_SISTEMA_PESSOA ON TN_T_SISTEMA_PESSOA.id_pessoa = TN_T_PESSOA.id
+        LEFT JOIN TN_T_SISTEMA ON TN_T_SISTEMA.id = TN_T_SISTEMA_PESSOA.id_sistema
+      WHERE 
+        COALESCE(TN_T_SISTEMA_PESSOA.dt_deleted, TN_T_SISTEMA.dt_deleted) IS NULL
+      GROUP BY TN_T_PESSOA.id, TN_T_PESSOA.ds_nome
+
       `);
 
       return resultado;
