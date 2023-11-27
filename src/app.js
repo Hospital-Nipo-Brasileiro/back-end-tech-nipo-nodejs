0c253a55c117926bdcae6defd6525b8e73d120a1
Sup Gustavo de Souza Fonseca <sup.gustavo@nipo.local>
2023-11-27 17:37:07 -0300

    [:recycle:] ajustando cors

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 require("dotenv").config();
 const express = require("express");
 const routes = require("./routes/index.js");
@@ -31,4 +32,36 @@ app.use(permissionador);
 app.use(manipulador404);
 app.use(manipuladorDeErros);
 
+=======
+const express = require("express");
+const routes = require("./routes/index.js");
+const manipulador404 = require("./middlewares/manipulador404.js");
+const manipuladorDeErros = require("./middlewares/manipuladorDeErros.js");
+const verifyToken = require("./middlewares/autenticador.js");
+const extractUserId = require("./middlewares/autenticador.js");
+const login = require("../src/routes/LoginRoute.js");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+
+const corsOptions = {
+  origin: ["http://10.10.204.54:3000", "http://10.10.204.54:8080"], 
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true, 
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+
+app.use(login);
+app.use(verifyToken);
+app.use(extractUserId);
+
+routes(app);
+
+app.use(manipulador404);
+app.use(manipuladorDeErros);
+
+>>>>>>> 8ac4f93 ([:recycle:] ajustando cors)
 module.exports = app;
