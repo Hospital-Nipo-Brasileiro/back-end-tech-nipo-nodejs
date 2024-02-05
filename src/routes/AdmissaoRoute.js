1e59aadc0d326a8c888814d72a491a2c244fad40
Sup Gustavo de Souza Fonseca <sup.gustavo@nipo.local>
2024-02-05 16:57:30 -0300

    [ :sparkles: ] Adicionando funcao de criar acesso automatico ao importar planilha

--- src/routes/AdmissaoRoute.js
+++ src/routes/AdmissaoRoute.js
@@ -8,6 +8,6 @@ const upload = multer({ dest: "./src/uploads-admissoes/" });
 router
   .post("/admissoes/enviar", upload.single("file"), AdmissaoController.previsualizaPlanilha)
   .post("/admissoes/desk", upload.single("file"), AdmissaoController.formataPlanilha, AdmissaoController.autenticar, AdmissaoController.criarDeskManager)
-  .post("/admissoes/concluir", upload.single("file"), AdmissaoController.formataPlanilha, AdmissaoController.concluirAdmissao);
+  .post("/admissoes/concluir", upload.single("file"), AdmissaoController.formataPlanilha, AdmissaoController.criaUmaPessoaAutomatico, AdmissaoController.concluirAdmissao);
   
 module.exports = router;
