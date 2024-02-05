0907cf2611e14a0201e31f79d30e22d803c7026c
Sup Gustavo de Souza Fonseca <sup.gustavo@nipo.local>
2024-02-05 17:00:13 -0300

    [ :seedling: ] Ajustando seed

--- src/seeders/20231117061801-demo_tn_t_permissao.js
+++ src/seeders/20231117061801-demo_tn_t_permissao.js
@@ -3,14 +3,26 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert("TN_T_PERMISSAO", [
       {
-        ds_nome: "leitura-estoque",
-        ds_descricao: "Permite a leitura de estoques",
+        ds_nome: "R-ADMIN",
+        ds_descricao: "Permite a leitura de logins",
         dt_created: new Date(),
         dt_updated: new Date()
       },
       {
-        ds_nome: "criacao-estoque",
-        ds_descricao: "Permite a criação de estoques",
+        ds_nome: "W-ADMIN",
+        ds_descricao: "Permite a criação de login",
+        dt_created: new Date(),
+        dt_updated: new Date()
+      },
+      {
+        ds_nome: "U-ADMIN",
+        ds_descricao: "Permite a atualização de login",
+        dt_created: new Date(),
+        dt_updated: new Date()
+      },
+      {
+        ds_nome: "D-ADMIN",
+        ds_descricao: "Permite deletar login",
         dt_created: new Date(),
         dt_updated: new Date()
       },
