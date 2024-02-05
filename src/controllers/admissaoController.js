d9885aab6c9669f5e3fb053edc6eea7f47038464
Sup Gustavo de Souza Fonseca <sup.gustavo@nipo.local>
2024-02-05 16:56:40 -0300

    [ :sparkles: ] Adicionando funcao de criar acesso automatico ao importar planilha

--- src/controllers/admissaoController.js
+++ src/controllers/admissaoController.js
@@ -2,12 +2,17 @@
 const { default: axios } = require("axios");
 const AdmissaoService = require("../services/admissaoService.js");
 const DeskManagerService = require("../services/deskManagerService.js");
+const PessoasController = require("../controllers/pessoasController.js");
+const database = require("../models");
 const os = require("os");
 
 require("dotenv").config();
 
 const fs = require("fs");
 const { promisify } = require("util");
+const ErroBase = require("../errors/ErroBase.js");
+const SistemaPessoaController = require("./sistemaPessoaController.js");
+const NaoEncontrado = require("../errors/NaoEncontrado.js");
 const writeFileAsync = promisify(fs.writeFile);
 
 class AdmissaoController {
@@ -52,7 +57,6 @@ class AdmissaoController {
       const usersReceived = await AdmissaoService.criarFormatacaoAcessos(users, diaAdmissao);
 
       req.fileUsers = usersReceived;
-      console.log(usersReceived);
       next();
       
     } catch (err) {
@@ -121,6 +125,66 @@ class AdmissaoController {
     }
   }
 
+  static async criaUmaPessoaAutomatico (req, _, next) {
+    const usersReceived = req.fileUsers;
+  
+    try {
+      for(const user of usersReceived) {
+        const cpf = user.cpf;
+    
+        const novaPessoa = {
+          ds_nome: user.nome,
+          nr_cpf: cpf,
+          dt_admissao: new Date(),
+          tp_contrato: user.tipoContrato,
+          dt_created: new Date(),
+          dt_updated: new Date(),
+        };
+    
+        try {
+          const pessoaEncontrada = await database.TN_T_PESSOA.findOne({ where: {nr_cpf: Number(cpf)}});
+    
+          if (pessoaEncontrada) {
+            next(new ErroBase(`Já existe uma pessoa com este cpf ${cpf}`, 409));
+          }
+          
+          const novaPessoaCriada = await database.TN_T_PESSOA.create(novaPessoa);
+          
+          for(const sistema of user.acessos) {
+            const idSistema = await database.TN_T_SISTEMA.findOne({ where: { ds_nome: sistema } });
+
+            if(!idSistema) {
+              if (sistema.length > 1) {
+                await database.TN_T_PESSOA.destroy({ where: {id: Number(novaPessoaCriada.id)}});
+                next(new NaoEncontrado(`Acesso não encontrado pelo nome ${sistema}, apagando pessoa pré-criada`));
+              } else {
+                next(new NaoEncontrado(`Acesso não encontrado pelo nome ${sistema}.`));
+              }
+            } else {
+              const novoSistemaPorPessoa = {
+                id_pessoa: novaPessoaCriada.id,
+                id_sistema: idSistema.id,
+                ds_usuario: user.usuario,
+                ds_senha: user.senha,
+                dt_created: new Date(),
+                dt_updated: new Date(),
+              };
+  
+              await database.TN_T_SISTEMA_PESSOA.create(novoSistemaPorPessoa);
+            }
+          }
+        } catch (err) {
+          console.error(err);
+          next(err);
+        }
+      }
+      return next();
+    } catch (err) {
+      console.error(err);
+      next(err);
+    }
+  }
+
   static async concluirAdmissao(req, res, next) {
     const usersReceived = req.fileUsers;
     const usuarioDeRede = os.userInfo().username;
@@ -140,6 +204,9 @@ class AdmissaoController {
       next(err);      
     }
   }
+  
 }
+
+
 
 module.exports = AdmissaoController;
