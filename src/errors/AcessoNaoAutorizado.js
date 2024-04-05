66084040087d869110a2c6c684508a95e7083ddc
Sup Gustavo de Souza Fonseca <sup.gustavo@nipo.local>
2024-04-05 08:42:35 -0300

    [ :fire: ] Remove everything

--- src/errors/AcessoNaoAutorizado.js
+++
@@ -1,9 +0,0 @@
-const ErroBase = require("./ErroBase.js");
-
-class AcessoNaoAutorizado extends ErroBase {
-  constructor(message = "Você não tem permissões de acesso") {
-    super(message, 403);
-  }
-}
-
-module.exports = AcessoNaoAutorizado;
--- src/errors/NaoEncontrado.js
+++
@@ -1,9 +0,0 @@
-const ErroBase =  require("./ErroBase.js");
-
-class NaoEncontrado extends ErroBase {
-  constructor (message = "Página não encontrada"){
-    super(message, 404);
-  }
-}
-
-module.exports = NaoEncontrado;
